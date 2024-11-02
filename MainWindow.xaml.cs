3bd67d9b523c93ce42f662d8b31623955ad3e5bb
Filip Bodziony <fbodziony6@ucn.zsem.edu.pl>
2024-11-02 20:21:31 +0100

    Initial Commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+
+namespace kotki_wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        int number = 1;
+        int lastNumber = 1;
+        const int minKotValue = 1;
+        const int maxKotValue = 4;
+
+        //int maxKotValue = countFiles();
+        //private static int countFiles()
+        //{
+        //    string[] files = System.IO.Directory.GetFiles("assets", "*.jpg");
+        //    return files.Length;
+        //}
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void btnPREV_Click(object sender, RoutedEventArgs e)
+        {
+            changePicture(-1);
+        }
+
+        private void btnNEXT_Click(object sender, RoutedEventArgs e)
+        {
+            changePicture(1);
+        }
+
+        private void changeNumber(int i)
+        {
+            if (i != 0)
+            {
+                number += i;
+                changeToCorrectRange(maxKotValue, minKotValue);
+            }
+            else
+            {
+                changeToCorrectRange(minKotValue, maxKotValue);
+            }
+        }
+
+        private void changeToCorrectRange(int first, int last)
+        {
+            if (number < minKotValue)
+            {
+                number = first;
+            }
+            else if (number > maxKotValue)
+            {
+                number = last;
+            }
+        }
+
+        private void changePicture(int i)
+        {
+            if (int.TryParse(txtNumber.Text, out number))
+            {
+                changeNumber(i);
+                lastNumber = number;
+                txtNumber.Text = number.ToString();
+                changeImageSource(number);
+            }
+            else
+            {
+                changeImageSource(lastNumber);
+            }
+
+        }
+
+        private void changeImageSource(int num)
+        {
+            imgKotek.Source = new BitmapImage(new Uri("assets/kot" + num + ".jpg", UriKind.Relative));
+        }
+
+        private void txtNumber_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            changePicture(0);
+        }
+
+        private void cbColor_Checked(object sender, RoutedEventArgs e)
+        {
+            changeBackgroundColor(gridContent, "#1565c0");
+        }
+
+        private void cbColor_Unchecked(object sender, RoutedEventArgs e)
+        {
+            changeBackgroundColor(gridContent, "#00796B");
+        }
+        private void changeBackgroundColor(System.Windows.Controls.Grid gridName, String colorString)
+        {
+            Color color = (Color)ColorConverter.ConvertFromString(colorString);
+            gridName.Background = new SolidColorBrush(color);
+        }
+    }
+}
---
+++ _XAMARIN/KOTEK/KOTEK/MainPage.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace KOTEK
+{
+    public partial class MainPage : ContentPage
+    {
+        bool isToggeledColor = false;
+        int photoNumber = 1;
+        public MainPage()
+        {
+            InitializeComponent();
+            changePicture(1);
+        }
+
+        private void changePicture(int n)
+        {
+            string link = "KOTEK.assets.kot" + n + ".jpg";
+            img.Source = ImageSource.FromResource(link);
+        }
+
+        private void btnPREV_Clicked(object sender, EventArgs e)
+        {
+            photoNumber--;
+            changeNumberPic();
+        }
+
+        private void checkNumber()
+        {
+            if (photoNumber <= 0)
+            {
+                photoNumber = 4;
+            }
+            else if (photoNumber > 4)
+            {
+                photoNumber = 1;
+            }
+        }
+
+        private void btnNEXT_Clicked(object sender, EventArgs e)
+        {
+            photoNumber++;
+            changeNumberPic();
+
+        }
+
+        private void changeNumberPic()
+        {
+            checkNumber();
+            changePicture(photoNumber);
+        }
+
+        private void changeColor()
+        {
+            if (isToggeledColor)
+            {
+                slWindow.BackgroundColor = Color.FromHex("#1565C0");
+            }
+            else
+            {
+                slWindow.BackgroundColor = Color.FromHex("#00796B");
+
+            }
+        }
+
+        private void swBackground_Toggled(object sender, ToggledEventArgs e)
+        {
+            if (swBackground.IsToggled)
+            {
+                isToggeledColor = true;
+            }
+            else
+            {
+                isToggeledColor = false;
+            }
+            changeColor();
+        }
+
+        private void nrImg_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string liczba = nrImg.Text.ToString();
+            string link = "KOTEK.assets.kot" + liczba + ".jpg";
+            img.Source = ImageSource.FromResource(link);
+        }
+    }
+}
